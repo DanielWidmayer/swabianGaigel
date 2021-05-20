85bc806a490613a6a90ed38664e65ab4f23de73c
JBHRgm <62023324+JBHRgm@users.noreply.github.com>
2021-05-20 18:44:44 +0200

    trial

--- config/env/production.js
+++ config/env/production.js
@@ -225,10 +225,10 @@ module.exports = {
          * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
          *                                                                          *
          ***************************************************************************/
-        // onlyAllowOrigins: [
-        //   'https://example.com',
-        //   'https://staging.example.com',
-        // ],
+        onlyAllowOrigins: [
+           'https://example.com',
+           'https://staging.example.com',
+        ],
         /***************************************************************************
          *                                                                          *
          * If you are deploying a cluster of multiple servers and/or processes,     *
