9a7b3d6f86d00cb07b0598c14d0d3b0f93cbb468
Daniel <57903719+DanielWidmayer@users.noreply.github.com>
2021-05-21 01:35:47 +0200

    update Chat styling, msg

--- assets/js/gameChat.js
+++ assets/js/gameChat.js
@@ -1,38 +1,51 @@
+// Global Chat Vars
 const chf = $("#chatfield");
 const gamef = $("#gamehistoryfield");
 const fpost = $("#fpost");
 const bpost = $("#bpost");
 
+// Automatic Scroll if user is at bottom of chat
+function appendMessage(text, field) {
+    field.append(text);
+    field.animate({ scrollTop: field[0].scrollHeight }, 500);
+}
+
+// Chat Socket Events
 io.socket.on("joinmsg", function (data) {
-    console.log("joined");
-    chf.append(`<p style="font-weigth: bold;">${data.user} ${data.text}</p>`);
+    appendMessage(`<p style="font-weigth: bold;">${data.user} ${data.text}</p>`, chf);
 });
 
 io.socket.on("leavemsg", function (data) {
     // data.user = username, data.text = message, data.bot = botname
-    console.log("left");
-    chf.append(`<p style="font-weigth: bold;">${data.user} ${data.text} ${data.bot}</p>`);
+    appendMessage(`<p style="font-weigth: bold;">${data.user} ${data.text} ${data.bot}</p>`, chf);
 });
 
 io.socket.on("controllermsg", function (data) {
-    console.log(data.msg);
-    gamef.append(`<p>${data.msg}</p>`);
+    appendMessage(`<p>${data.msg}</p>`, gamef);
 });
 
 io.socket.on("chatmsg", function (data) {
-    console.log(data.text);
-    chf.append(`<div class="chatmsg"><b>${data.user}:&nbsp;</b>${data.text}</div>`);
+    appendMessage(`<div class="chatmsg"><b>${data.user}:&nbsp;</b>${data.text}</div>`, chf);
 });
 
 io.socket.on("turnmsg", function (data) {
-    console.log(data);
     let text;
     if (userHash == data.user.hashID) {
-        text = '<p>You have won the trick.</p><p class="mb-3">Your score is now ' + data.user.score + '</p><hr class="mt-0"/><p>It\'s your turn.</p>';
+        text = "<p>You have won the trick.</p><p>Your score is now " + data.user.score + '</p><hr class="hr-thick"/><p>It\'s your turn.</p>';
     } else {
-        text = "<p>" + data.user.name + ' has won the trick.</p><p class="mb-3">' + data.user.name + "'s score is now " + data.user.score + '</p><hr class="mt-0"/><p>It\'s ' + data.user.name + "'s turn.</p>";
+        text = "<p>" + data.user.name + " has won the trick.</p><p>" + data.user.name + "'s score is now " + data.user.score + '</p><hr class="hr-thick"/><p>It\'s ' + data.user.name + "'s turn.</p>";
     }
-    gamef.append(`${text}`);
+    appendMessage(`${text}`, gamef);
+});
+
+io.socket.on("firstturnmsg", function (data) {
+    let text;
+    if (userHash == data.user.hashID) {
+        text = "<p>It's your turn to start the game.</p>";
+    } else {
+        text = `<p>It\'s ${data.user.name}'s turn to start the game.</p>`;
+    }
+    appendMessage(`${text}`, gamef);
 });
 
 io.socket.on("cardplayedmsg", function (data) {
@@ -44,18 +57,18 @@ io.socket.on("cardplayedmsg", function (data) {
     } else {
         text = `${data.user.name} has`;
     }
-    gamef.append(`<p>${text} played a ${icon} ${cardletter}</p>`);
+    appendMessage(`<p>${text} played a ${icon} ${cardletter}</p>`, gamef);
 });
 
 io.socket.on("paircalledmsg", function (data) {
     let text;
     let icon = getHtmlSymbol(data.symbol);
     if (userHash == data.user.hashID) {
-        text = `<hr class="mt-0"/><p>You have melded in ${icon}.</p><p class="mb-3">Your score is now ${data.user.score}</p><hr class="mt-0"/>`;
+        text = `<hr class="mb-0"/><p>You have melded in ${icon}.</p><p>Your score is now ${data.user.score}</p><hr class="mt-0"/>`;
     } else {
-        text = `<hr class="mt-0"/><p>${data.user.name} has melded in ${icon}.</p><p class="mb-3">${data.user.name}'s score is now ${data.user.score}</p><hr class="mt-0"/>`;
+        text = `<hr class="mb-0"/><p>${data.user.name} has melded in ${icon}.</p><p>${data.user.name}'s score is now ${data.user.score}</p><hr class="mt-0"/>`;
     }
-    gamef.append(`${text}`);
+    appendMessage(`${text}`, gamef);
 });
 
 io.socket.on("cardrobmsg", function (data) {
@@ -63,21 +76,25 @@ io.socket.on("cardrobmsg", function (data) {
     let icon = getHtmlSymbol(data.card.symbol);
     let cardletter = getCardLetter(data.card.value);
     if (userHash == data.user.hashID) {
-        text = `<hr class="mt-0"/><p>You have robbed the ${icon} ${cardletter}.</p><hr class="mt-0"/>`;
+        text = `<hr class="mb-0"/><p>You have robbed the ${icon} ${cardletter}.</p><hr class="mt-0"/>`;
     } else {
-        text = `<hr class="mt-0"/><p>${data.user.name} has robbed the ${icon} ${cardletter}.</p><hr class="mt-0"/>`;
+        text = `<hr class="mb-0"/><p>${data.user.name} has robbed the ${icon} ${cardletter}.</p><hr class="mt-0"/>`;
     }
-    gamef.append(`${text}`);
+    appendMessage(`${text}`, gamef);
 });
 
 io.socket.on("gameovermsg", function (data) {
     let text;
-    if (userHash == data.user.hashID) {
-        text = `<hr class="mt-0"/><p>Congratulations You have won the game!</p><hr class="mt-0"/>`;
+    if (data.users.find((el) => el.hashID == userHash)) {
+        text = `<hr class="mb-0"/><p>Congratulations You have won the game!</p><hr class="mt-0 hr-thick"/>`;
     } else {
-        text = `<hr class="mt-0"/><p>Game Over! ${data.user.name} has won the game!.</p><hr class="mt-0"/>`;
+        let winners = "";
+        for (const user of data.users) {
+            winners += user.name + " ";
+        }
+        text = `<hr class="mb-0"/><p>Game Over! ${winners} has won the game!</p><hr class="mt-0 hr-thick"/>`;
     }
-    gamef.append(`${text}`);
+    appendMessage(`${text}`, gamef);
 });
 
 io.socket.on("firstcardtypemsg", function (data) {
@@ -87,7 +104,7 @@ io.socket.on("firstcardtypemsg", function (data) {
     } else {
         text = `<p>${data.user.name} has called ${data.first_type}.</p>`;
     }
-    gamef.append(`${text}`);
+    appendMessage(`${text}`, gamef);
 });
 
 function getHtmlSymbol(symbol) {
@@ -141,7 +158,7 @@ function getCardLetter(value) {
     return cardletter;
 }
 
-bpost.click(postmsg);
+bpost.on("click", postmsg);
 
 fpost.keypress(function (e) {
     var keycode = e.keyCode ? e.keyCode : e.which;
