8ca0e87309dceb4116abd4fa8d2b9ee0f346f286
JBHRgm <62023324+JBHRgm@users.noreply.github.com>
2021-05-24 20:03:18 +0200

    userlist admin options

--- assets/js/userList.js
+++ assets/js/userList.js
@@ -1,7 +1,8 @@
 const ul = $("#userlist");
+var admin = false;
 
 io.socket.on("userevent", function (data) {
-    let playericon, playername, readyicon;
+    let playericon, playername, readyicon, kickbutton;
     ul.empty();
     data.users.forEach((user) => {
         playername = `${user.name}<span style="font-size: 0.85rem" class="px-2 text-secondary">#${user.hashID}</span>`;     //<------ JBHR --- start
@@ -14,11 +15,68 @@ io.socket.on("userevent", function (data) {
             if (user.ready) readyicon = '<i class="bi bi-check-circle-fill text-success px-1"></i>';
             else readyicon = '<i class="bi bi-x-circle-fill text-warning px-1"></i>';
         } else readyicon = "";
-        //ul.append(`${playericon}${user.name}${readyicon}<br>`);       
-        ul.append(`${readyicon}${playericon}${playername}<br>`);         //<---- JBHR ---- end
+        //ul.append(`${playericon}${user.name}${readyicon}<br>`); 
+        console.log(admin);
+        if (admin) {
+            kickbutton = `<button class="btn btn-danger" title="Kick Player" onclick="kickPlayer(${user.hashID});">
+                <i class="bi bi-x-square"></i></button>`;
+        } else kickbutton = "";
+        ul.append(`${readyicon}${playericon}${playername}${kickbutton}<br>`);         //<---- JBHR ---- end
     });
+    if (admin) {
+        if (data.users.length >= data.max) $('#bAddBot').hide();
+        else $('#bAddBot').show();
+    }
 });
 
-/* text =<div class="game-utils">
-<button id="bshuffle" class="btn btn-game btn-secondary" title="Shuffle players" onclick="shufflePlayers"><i class="bi bi-shuffle"></i> </button>
-</div> */
+
+io.socket.on("adminchange", function (data) {
+    ul.after(`<div>
+        <button id="bshuffle" class="btn btn-primary" title="Shuffle players" onclick="shuffle();">
+        <i class="bi bi-dice-4"></i> Randomize</button>
+        <button id="bAddBot" class="btn btn-warning" title="Add Bot" onclick="addBot();">
+        <i class="bi bi-person-square"></i> add Bot</button>
+    </div>`);
+    admin = true;
+})
+
+function shuffle() {
+    io.socket.post("/randomOrder", function (res, jres) {
+        if (jres.statusCode != 200) {
+            console.log(jres);
+        } else {
+            console.log(res);
+        }
+    });
+}
+
+function addBot() {
+    io.socket.post("/addBot", function (res, jres) {
+        if (jres.statusCode != 200) {
+            console.log(jres);
+        } else {
+            console.log(res);
+        }
+    });
+}
+
+function kickPlayer(userhash) {
+    io.socket.post("/kickPlayer", { target: userhash }, function (res, jres) {
+        if (jres.statusCode != 200) {
+            console.log(jres);
+        } else {
+            console.log(res);
+        }
+    });
+}
+
+function switchTeam() {
+    io.socket.post("/switchTeam", function (res, jres) {
+        if (jres.statusCode != 200) {
+            console.log(jres);
+        } else {
+            console.log(res);
+        }
+    });
+}
+
