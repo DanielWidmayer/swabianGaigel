2f97a3b00441040b90c085960b794cf6f95cf630
JBHRgm <62023324+JBHRgm@users.noreply.github.com>
2021-05-25 01:37:06 +0200

    teams and admin functions added

--- api/controllers/GameController.js
+++ api/controllers/GameController.js
@@ -45,26 +45,20 @@ module.exports = {
                 temp[i] = temp[j];
                 temp[j] = m;
             }
-
-            if (temp.length == 4) {
+            
+            if (room.maxplayers >= 4) {
+                let div = Math.floor(room.maxplayers / 2);
                 for (let i = 0; i < temp.length; i++) {
-                    temp[i].team = (i % 2) + 1;
+                    temp[i].team = (i % div) + 1;
                 }
-            } else if (temp.length == 6) {
-                for (let i = 0; i < temp.length; i++) {
-                    temp[i].team = (i % 3) + 1;
-                }
-            }
-
-            for (let pl of temp) {
-                t_player = await User.getNameAndHash(pl.playerID);
-                if (t_player.bot) pl.ready = true;
-                else pl.ready = false;
             }
 
             room.jsonplayers = temp;
-            for (const pl of room.jsonplayers) {
-                players.push(await User.getNameAndHash(pl.playerID));
+            for (let pl of room.jsonplayers) {
+                t_player = await User.getNameAndHash(pl.playerID);
+                if (t_player.bot) pl.ready = true;
+                else pl.ready = false;
+                players.push(t_player);
                 players[players.length - 1].ready = pl.ready;
                 players[players.length - 1].team = pl.team;
             }
@@ -102,6 +96,9 @@ module.exports = {
             if (!room) throw error(101, "This Room could not be found!");
             if (!user) throw error(101, "This User could not be found!");
 
+            // check if game is already running
+            if (room.status == "game") throw error(104, "The game has already started!");
+
             // check if user is in room
             let p_index = room.jsonplayers.findIndex((pl) => pl.playerID == user.id);
             if (p_index < 0) throw error(101, "User is not in this room!");
@@ -110,7 +107,7 @@ module.exports = {
             let t_team = req.body.team;
             if (t_team < 0 || t_team > 3) throw error(104, "This Team does not exist!");
 
-            if (room.jsonplayers.length < 4) throw error(104, "No Teams allowed for less than 4 players!");
+            if (room.maxplayers < 4) throw error(104, "No Teams allowed for less than 4 players!");
 
             for (const pl of room.jsonplayers) {
                 teams[pl.team] += 1;
@@ -129,7 +126,7 @@ module.exports = {
                 players[players.length - 1].team = el.team;
             }
 
-            sails.sockets.broadcast(room.hashID, "userevent", { users: players, max: room.maxplayers }, req);
+            sails.sockets.broadcast(room.hashID, "userevent", { users: players, max: room.maxplayers });
 
             return res.status(200).json({ team: room.jsonplayers[p_index].team });
         } catch (err) {
@@ -305,7 +302,25 @@ module.exports = {
                     sails.sockets.broadcast(room.hashID, "userevent", { users: players, max: room.maxplayers });
                     return res.status(200).json({ ready: room.jsonplayers[u_index].ready });
                 }
-                if ([2, 3, 4, 6].includes(room.jsonplayers.length) == false) throw error(102, `Can't start a game with ${room.jsonplayers.length} players!`);
+
+                if (room.jsonplayers.length < room.maxplayers) {
+                    // fill up with Bots
+                    for (let i = room.jsonplayers.length; i < room.maxplayers; i++) {
+                        let newbot = await User.newBot();
+                        room.jsonplayers.push({
+                            playerID: newbot.id,
+                            hand: [],
+                            score: 0,
+                            wins: 0,
+                            team: 0
+                        });
+                        newbot = await User.getNameAndHash(newbot.id);
+                        ChatController.botmsg(newbot.name, room.hashID, 1);
+                    }
+                    
+                    await Room.updateOne({ id: room.id }).set({ jsonplayers: room.jsonplayers });
+                    //sails.sockets.broadcast(room.hashID, "userevent", { users: t_players, max: room.maxplayers });
+                }
 
                 // update room status, reject if already ingame
                 if (room.status == "game") throw error(104, "Game is already running!");
@@ -354,6 +369,8 @@ module.exports = {
                     players.push(await User.getNameAndHash(pl.playerID));
                     players[players.length - 1].team = pl.team;
                 }
+                sails.sockets.broadcast(room.hashID, "userevent", { users: players, max: room.maxplayers });
+
                 for (const el of room.jsonplayers) {
                     sails.log("deal 5 cards to player " + el.playerID);
                     hand = await Card.dealCard(5, el.playerID, room.id);
@@ -1047,19 +1064,16 @@ async function applyWin(roomid, firstround, winnerID) {
             // get winner team
             for (let i = 0; i < players.length; i++) {
                 if (players[i].team == p_team) {
-                    p_win.push(players[i].playerID);
-                    p_win[p_win.length - 1].index = i;
+                    p_win.push(i);
                 }
             }
-            sails.log.info(p_win);
-            sails.log.info(players);
+
             for (const el of p_win) {
-                sails.log.info(el);
                 for (let i = 0; i < stack.length; i++) {
                     // gesamten Stich als Score hochzählen
-                    players[el.index].score += stack[i].card.value;
+                    players[el].score += stack[i].card.value;
                 }
-                players[el.index].wins += 1;
+                players[el].wins += 1;
             }
 
             user = await User.getNameAndHash(players[winner].playerID);
@@ -1068,8 +1082,8 @@ async function applyWin(roomid, firstround, winnerID) {
 
             sails.sockets.broadcast(room.hashID, "roundwin", { user: user });
 
-            if (p_win[0].index == acPl) acPl = p_win[1].index;
-            else acPl = p_win[0].index;
+            if (p_win[0] == acPl) acPl = p_win[1];
+            else acPl = p_win[0];
         }
         sails.log(user.name + " won!");
 
